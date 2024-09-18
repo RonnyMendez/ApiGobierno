376cc80b059925ca349d89316019d1f7e1210d96
ronn <rmendezf@unitru.edu.pe>
2024-09-18 15:56:15 -0500

    Proyecto Creado

---
+++ src/main/java/com/gobierno/backend/GobiernoApplication.java
@@ -0,0 +1,13 @@
+package com.gobierno.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobiernoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GobiernoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gobierno/backend/GobiernoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gobierno.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GobiernoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
